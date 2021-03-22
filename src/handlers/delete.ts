89093a321aef7dc0d797ae379adaa97bfaf9f0d6
tvisconti <acometappears@gmail.com>
2021-03-22 15:25:12 -0700

    fixed delete

--- src/handlers/delete.ts
+++ src/handlers/delete.ts
@@ -1,17 +1,7 @@
 import {APIGatewayEvent, Context} from 'aws-lambda';
 import { doesUserExist, deleteUser } from '../data/users';
-import User from '../models/User';
 
-export async function handler(event: APIGatewayEvent, context: Context) {
-    if (!event || !event.body || !(JSON.parse(event.body) instanceof User)) {
-        return JSON.stringify({
-            statusCode: 400,
-            body: {
-                'error': 'Invalid request.'
-            }
-        });
-    }
-    
+export async function handler(event: APIGatewayEvent, context: Context) {    
     const pathParameters = event.pathParameters;
 
     if(!pathParameters || pathParameters.username === undefined) {
